f4289d99ded4f760ad25f6a5942262838ecd82b5
HolstCh <chad.holst@Hotmail.com>
2023-11-17 02:44:11 -0700

    JS for clicking on an image on movies page and rendering the corresponding data on description page

---
+++ HTPC/scripts/moviesData.js
@@ -0,0 +1,17 @@
+const moviesData = [
+    {
+        title: "Spider-Man: Across the Spider-Verse",
+        description: "Miles Morales catapults across the Multiverse, where he encounters a team of Spider-People charged with protecting its very existence. When the heroes clash on how to handle a new threat, Miles must redefine what it means to be a hero.",
+        year: "2023",
+        ageRating:"PG-13",
+        duration:"2h 20m",
+        tags: ["Animation", "Action", "Adventure"],
+        userRating: "",
+        totalRatings:"50,000",
+        src:"../images/spiderverse-poster.jpg",
+        subscribed:["../images/netflix.png"],
+        unsubscribed:["../images/amazonicon.png", "../images/appletv.png"],
+    },
+];
+
+export default moviesData;
