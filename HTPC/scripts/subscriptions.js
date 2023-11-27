26fe4416694e9213b3d985b93e9728762db4cf72
williamchan2 <87112587+williamchan2@users.noreply.github.com>
2023-11-26 19:59:15 -0700

    logic for multiple apps user flow implemented

--- HTPC/scripts/subscriptions.js
+++ HTPC/scripts/subscriptions.js
@@ -6,9 +6,24 @@ function dropdownFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
+
+function dropdownFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.subscribeButtonDisney')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+    if (!event.target.matches('.subscribeButtonNetflix')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -21,45 +36,69 @@ function dropdownFunction() {
   }
 
 // subscribe button (dropdown) and unsubscribe button 
-const subButton = document.querySelector(".dropbtn")
-const showUnsubscribe = document.querySelector(".unsubscribeButton")
+const subscribeButtonNetflix = document.querySelector(".subscribeButtonNetflix")
+const subscribeButtonDisney = document.querySelector(".subscribeButtonDisney")
+const showUnsubscribe = document.querySelector(".unsubscribeButtonNetflix")
+const showUnsubscribe1 = document.querySelector(".unsubscribeButtonDisney")
 
 
-function unsubConfirm() {
+
+function unsubConfirm(name) {
   let text = "Are you sure you want to unsubscribe?";
   if (confirm(text) == true) {
     text = "Unsubscribed <br> No Current Plan"
-    disneyHeader.innerHTML = text;
-    showUnsubscribe.setAttribute("hidden", "hidden");
-    subButton.textContent = "Subscribe";
-    subButton.style.marginTop = "55px"
+
+    if (String(name).valueOf() === "netflix") {
+      netflixHeader.innerHTML = text;
+      showUnsubscribe.setAttribute("hidden", "hidden");
+      subscribeButtonNetflix.textContent = "Subscribe";
+      subscribeButtonNetflix.style.marginTop = "55px"
+    }
+    else if (String(name).valueOf() === "disney+") {
+      disneyHeader.innerHTML = text;
+      showUnsubscribe1.setAttribute("hidden", "hidden");
+      subscribeButtonDisney.textContent = "Subscribe";
+      subscribeButtonDisney.style.marginTop = "55px"
+    }
+
   }
 
 }
 
 
-// i am sorry... this is disgusting
+function subscribeOnClick(name) {
+
+  console.log(name);
+  if (String(name).valueOf() === "netflix") {
+  subscribeButtonNetflix.textContent = "Edit Plan"; // change button text to say edit plan since we are now subscribed
+  subscribeButtonNetflix.style.marginTop = "25px"; // move button location so it's aligned evenly
+  showUnsubscribe.removeAttribute("hidden"); // show unsubscribe button now that we are subscribed 
+  }
+  else if (String(name).valueOf() === "disney+") {
+    subscribeButtonDisney.textContent = "Edit Plan"; // change button text to say edit plan since we are now subscribed
+    subscribeButtonDisney.style.marginTop = "25px"; // move button location so it's aligned evenly
+    showUnsubscribe1.removeAttribute("hidden"); // show unsubscribe button now that we are subscribed 
+
+  }
+}
+
+//netflix 
+const netflixHeader = document.querySelector("#netflix-header")
+document.querySelector("#netflix6").addEventListener("click", function() {netflixHeader.innerHTML = "Subscribed <br> Current Plan: $6/month"})
+document.querySelector("#netflix17").addEventListener("click", function() {netflixHeader.innerHTML = "Subscribed <br> Current Plan: $17/month"})
+document.querySelector("#netflix21").addEventListener("click", function() {netflixHeader.innerHTML = "Subscribed <br> Current Plan: $21/month"})
 
 // disney plus 
 const disneyHeader = document.querySelector("#disney-header")
-
 // $8 plan
 const disney8 = document.querySelector("#disney8");
 disney8.addEventListener("click", function() {disneyHeader.innerHTML = "Subscribed <br> Current plan: $8/month";}); // first change header text 
-disney8.addEventListener("click", function() {subButton.textContent = "Edit Plan";}); // change button text to say edit plan since we are now subscribed
-disney8.addEventListener("click", function(){subButton.style.marginTop = "25px"}); // move button location so it's aligned evenly
-disney8.addEventListener("click", function(){showUnsubscribe.removeAttribute("hidden");}); // show unsubscribe button now that we are subscribed 
 // $12 plan
 const disney12 = document.querySelector("#disney12");
 disney12.addEventListener("click", function() {disneyHeader.innerHTML = "Subscribed <br> Current plan: $12/month";});
-disney12.addEventListener("click", function() {subButton.textContent = "Edit Plan";});
-disney12.addEventListener("click", function(){subButton.style.marginTop = "25px"});
-disney12.addEventListener("click", function(){showUnsubscribe.removeAttribute("hidden");});
 // $15 plan
 const disney15 = document.querySelector("#disney15");
 disney15.addEventListener("click", function() {disneyHeader.innerHTML = "Subscribed <br> Current plan: $15/month";}); 
-disney15.addEventListener("click", function() {subButton.textContent = "Edit Plan";});
-disney15.addEventListener("click", function(){subButton.style.marginTop = "25px"});
-disney15.addEventListener("click", function(){showUnsubscribe.removeAttribute("hidden");});
+
 
 
